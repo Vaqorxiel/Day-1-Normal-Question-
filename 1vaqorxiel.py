a3052baa1808b2d2d2e63c69249b1b80fa5691c5
Vaqorxiel <princebrecio5@gmail.com>
2026-06-01 22:11:01 +0800

    Create 1vaqorxiel.py
    Day 1-first python program done

---
+++ 1vaqorxiel.py
@@ -0,0 +1,8 @@
+# Day 1 - Age 14 - Vaqorxiel
+# First Python program - Road to IBM
+
+name = input("What's your name? ")
+print("Hello!!", name)
+print("I am Vaqorxiel")
+print("Age 14, learning Python")
+print("Day 1 complete - ThinkPad at 18")
