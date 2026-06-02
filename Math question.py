260fe7927a227cc3a70ac55bcc2c92a1cd622548
Vaqorxiel <princebrecio5@gmail.com>
2026-06-02 14:46:16 +0800

    Create Math question.py
    Helloworld("print")
    *Ohio confident*

---
+++ Math question.py
@@ -0,0 +1,19 @@
+score=0
+print("math question easy-hard")
+number=input("whats 2+2?:")
+
+if number=="4":
+    print("good")
+    score += 1
+else:
+    print("SON")
+    
+plot=input("whats 33.5+33.5?:")
+
+if plot=="67":
+           print("*ohio respect*")
+           score += 1
+else:
+           print("priSON")
+           
+print(f"game over. final score:{score}/2")
