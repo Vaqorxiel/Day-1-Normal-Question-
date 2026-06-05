c064ad124e19c9c3fc4194236420f968a7c3fcff
Vaqorxiel <princebrecio5@gmail.com>
2026-06-05 13:54:35 +0800

    Implement basic chatbot functionality in funnybot.py
    Create a simple interactive chatbot that engages users in conversation and counts to 6.

---
+++ funnybot.py
@@ -0,0 +1,35 @@
+answer=input("do you want to talk to me?:")
+if answer.lower()=="yes":
+        print("okay thats good")
+else:
+        print("okay you can close the program then,just kidding but!")
+ 
+age=int(input("how old are you?:"))
+print("your gonna be",age+1,"soon! advace happy bday!!!!")
+
+answer=input("im scared do you think i can do this?:")
+print("okay...")
+
+answer=input("i will count 1-5:")
+
+def count():
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
+    print(6)
+
+for i in range(1):
+    count()
+    
+answer=input("do you want me to continue?:")
+if answer=="yes":
+    print("7")
+else:
+    print("6.....ss.sssevv....plsss c..ccontinuee~~~")
+
+plot=input("continue?:")
+print("67")
+plot=input("whats you reaction?, do you feel like you want to fart?:")
+print("ohh okayy...")
